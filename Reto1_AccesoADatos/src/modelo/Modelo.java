ca9d4b6311ccbc7b2f3fde80efa121ece7f2bc24
mitikosander <alexloveparty@gmail.com>
2019-10-22 12:52:12 +0200

    Creación del proyecto

---
+++ Reto1_AccesoADatos/src/modelo/Modelo.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Modelo {
+
+	private Fichero fichero;
+	private BBDD bbdd;
+	
+	public Modelo() {
+		setFichero(new Fichero());
+		setBbdd(new BBDD());
+	}
+
+	public Fichero getFichero() {
+		return fichero;
+	}
+
+	public void setFichero(Fichero fichero) {
+		this.fichero = fichero;
+	}
+
+	public BBDD getBbdd() {
+		return bbdd;
+	}
+
+	public void setBbdd(BBDD bbdd) {
+		this.bbdd = bbdd;
+	}
+	
+}
