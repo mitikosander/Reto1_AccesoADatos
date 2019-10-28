91794ea285a4072374f5c02dab4e47c493514775
admin1 <admin1@DESKTOP-J0KSH16>
2019-10-28 12:20:21 +0100

    Clase para leer el fichero txt

---
+++ Reto1_AccesoADatos/src/modelo/Empleado.java
@@ -0,0 +1,72 @@
+package modelo;
+
+public class Empleado {
+	private int codigo;
+	private String nombre;
+	private String apellido;
+	private String fecha_creacion;
+	private int cod_dpto;
+	private int cod_jefe;
+	
+	Empleado(){
+		
+	}
+
+	public Empleado(int codigo, String nombre, String apellido, String fecha_creacion, int cod_dpto, int cod_jefe) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fecha_creacion = fecha_creacion;
+		this.cod_dpto = cod_dpto;
+		this.cod_jefe = cod_jefe;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFecha_creacion() {
+		return fecha_creacion;
+	}
+
+	public void setFecha_creacion(String fecha_creacion) {
+		this.fecha_creacion = fecha_creacion;
+	}
+
+	public int getCod_dpto() {
+		return cod_dpto;
+	}
+
+	public void setCod_dpto(int cod_dpto) {
+		this.cod_dpto = cod_dpto;
+	}
+
+	public int getCod_jefe() {
+		return cod_jefe;
+	}
+
+	public void setCod_jefe(int cod_jefe) {
+		this.cod_jefe = cod_jefe;
+	}
+}
