43ff3442eb536b27378178f77e72fdcfca8ffc70
admin1 <admin1@DESKTOP-ITAUFUT>
2019-10-29 13:37:34 +0100

    Correción en Departamento y Empleado y metodos Insert dpto y emp

--- Reto1_AccesoADatos/src/ejecutar/Main.java
+++ Reto1_AccesoADatos/src/ejecutar/Main.java
@@ -2,6 +2,7 @@ package ejecutar;
 import java.sql.Connection; //TODO borrar despues de probar la conexion
 
 import controlador.Controlador;
+import modelo.Empleado;
 import modelo.Modelo;
 import vista.Vista;
 public class Main {
@@ -12,8 +13,7 @@ public class Main {
 		Vista vista = new Vista();
 		Controlador controlador = new Controlador(vista, modelo);
 		
-		modelo.getBbdd().conectar();
-		modelo.getBbdd().desconectar();
+		
 	}
 
 }
